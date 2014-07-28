3cc16c8b8abb902dfa7f79d65fd7913327c36d06
Ivan Konovalov <ivknv0@gmail.com>
2014-07-28 19:52:55 +0600

    Add DZone parser

---
+++ TechParser/dzone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import parser
+import grab
+
+def get_articles():
+	g = grab.Grab()
+	parser.setup_grab(g)
+	
+	g.go("http://www.dzone.com")
+	
+	css_path = ".linkblock .details h3 a[rel=bookmark]"
+	
+	posts = parser.get_articles(g, css_path, css_path, "dzone")
+	
+	for i in range(len(posts)):
+		link = posts[i]["link"]
+		
+		if link.startswith("/"):
+			link = "http://www.dzone.com" + link
+			posts[i]["link"] = link
+		posts[i]["title"] = posts[i]["title"].strip()
+	
+	return posts
