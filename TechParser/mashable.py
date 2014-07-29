d97125f8f9ac4f1e6fa73616c3a034e4de4e0064
Ivan Konovalov <ivknv0@gmail.com>
2014-07-29 21:19:10 +0600

    Add Mashable parser

---
+++ TechParser/mashable.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import parser
+import grab
+
+def get_articles():
+	g = grab.Grab()
+	
+	parser.setup_grab(g)
+	
+	g.go("http://mashable.com")
+	
+	css_path = "article header hgroup h1 a"
+	
+	posts = parser.get_articles(g, css_path, css_path,
+		"mashable", "mashable.com")
+	
+	return posts
