a742fd693755bc3158e759136190bbb3acfe9b4d
Ivan Konovalov <ivknv0@gmail.com>
2014-07-30 14:50:21 +0600

    Add Digg parser

---
+++ TechParser/digg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import parser
+import grab
+
+def get_articles():
+	g = grab.Grab()
+	parser.setup_grab(g)
+	
+	g.go("http://digg.com")
+	
+	css_path = ".story-container .story-content .story-header .story-title .story-title-link"
+	
+	posts = parser.get_articles(g, css_path, css_path,
+		"digg")
+	
+	return posts
