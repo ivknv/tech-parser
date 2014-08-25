524efd59de8d3e8eb1b02d236f7ed57fe18b3d10
Ivan Konovalov <ivknv0@gmail.com>
2014-08-25 23:40:25 +0600

    Another fix

--- TechParser/techrepublic.py
+++ TechParser/techrepublic.py
@@ -16,17 +16,10 @@ def get_articles():
 	css_paths = ["#article-river #tab-content-1 .items li .item-content .title a",
 		"#article-river #tab-content-2 .items li .item-content .title a",
 		"#haccordion .viewport section .slide .lead-in h3 a",
-		"#feature-hubs .item-list .row .item .title a",]
-	
-	summary_path = ".items li .item-content p.dek"
-	summary_texts = [parser.cut_text(parser.unicode_(text.text_content()).strip())
-		for text in g.css_list(summary_path)]
+		"#feature-hubs .item-list .row .item .title a"]
 	
 	for css_path in css_paths:
 		posts += parser.get_articles(g, css_path, css_path,
 			"techrepublic", "techrepublic.com")
-	
-	for (post, summary_text) in zip(posts, summary_texts):
-		post['summary'] = summary_text
 	
 	return posts
