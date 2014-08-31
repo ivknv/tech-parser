9868164b48bee85b16af2bae988b09c2cd86e8ec
SPython <rvan.mega@gmail.com>
2014-08-31 14:20:54 +0600

    Fix Slashdot parser

--- TechParser/slashdot.py
+++ TechParser/slashdot.py
@@ -10,13 +10,37 @@ def get_articles(start_page=1, end_page=5):
 	parser.setup_grab(g)
 	
 	posts = []
-	css_path = "article header h2 span a"
-	summary_path = "article .body .p"
+	css_path = "article"# header h2 span a"
+	title_path = "header h2 span a"
+	summary_path = ".body .p"
 	
-	for i in range(start_page, end_page+1):
+	for i in range(start_page-1, end_page):
 		g.go("http://slashdot.org?page=%i" %i)
 		
-		posts += parser.get_articles(g, css_path, css_path,
-			"slashdot", summary_path=summary_path)
+		for article in g.css_list(css_path):
+			article.make_links_absolute(g.response.url)
+			try:
+				link = article.cssselect(title_path)[0]
+			except IndexError:
+				continue
+			title = parser.escape_title(link.text_content())
+			link = link.get("href")
+			try:
+				summary = article.cssselect(summary_path)[0]
+				for i in summary:
+					for j in i.getchildren():
+						if j.tag in ['script', 'style']:
+							i.remove(j)
+				summary = parser.cut_text(summary.text_content().strip())
+			except IndexError:
+				summary = 'No summary text available.'
+			posts.append({'title': title,
+						'summary': summary,
+						'link': link,
+						'source': 'slashdot'})
+
+		
+#		posts += parser.get_articles(g, css_path, css_path,
+#			"slashdot", summary_path=summary_path)
 		
 	return posts
