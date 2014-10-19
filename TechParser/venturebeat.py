fdbecb16ec3b7245d782a5c953803983bf8a2c83
SPython <rvan.mega@gmail.com>
2014-10-19 20:20:34 +0600

    Improve VentureBeat parser

--- TechParser/venturebeat.py
+++ TechParser/venturebeat.py
@@ -1,34 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import grab
 from TechParser import parser
 
-def get_articles(start_page=1, end_page=3):
-	g = grab.Grab()
-	
-	parser.setup_grab(g)
-	
-	g.go("http://venturebeat.com")
-	
-	posts = []
-	
-	css_paths = [("#vb-featured li a", "span.snipe-wrapper h2.title"),
-		(".post .entry-wrapper .entry-header .entry-title a", ""),
-		("ul.most-popular li h2 a", ""),
-		("ul.editors-pick li h2 a", "")]
-	
-	summary_path = ".post .entry-wrapper .entry-summary p"
-	
-	if start_page == 1:
-		for (link_path, title_path) in css_paths:
-			posts += parser.get_articles(g,	link_path, link_path+title_path,
-				"venturebeat", "venturebeat.com")
-	
-	for i in range(start_page+1, end_page+1):
-		g.go("http://venturebeat.com/page/%i" %i)
-		
-		posts += parser.get_articles(g, css_paths[1][0], css_paths[1][0],
-			"venturebeat", "venturebeat.com", summary_path)
-	
-	return posts
+def get_articles():
+	return parser.get_articles_from_rss("http://venturebeat.com/feed",
+		'venturebeat')
