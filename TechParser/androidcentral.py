2611fe42ad1ca596f7451feb57e241837812e4fb
Ivan Konovalov <ivknv0@gmail.com>
2014-10-19 20:27:11 +0600

    Improve Android Central parser

--- TechParser/androidcentral.py
+++ TechParser/androidcentral.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
 from TechParser import parser
 
-def get_articles(start_page=1, end_page=5):
-	g = grab.Grab()
-	
-	parser.setup_grab(g)
-	
-	posts = []
-	css_path = ".node-article .node-inner .title-byline .title a"
-	summary_path = ".entry .entry-content .field .field-items"
-	
-	for i in range(start_page, end_page+1):
-		g.go("http://www.androidcentral.com/content?page=%i" %i)
-		
-		articles = parser.get_articles(g, css_path, css_path,
-			"androidcentral", "www.androidcentral.com", summary_path)
-		
-		posts += articles
-	
-	return posts
+def get_articles():
+	return parser.get_articles_from_rss('http://feeds2.feedburner.com/androidcentral',
+		'androidcentral')
