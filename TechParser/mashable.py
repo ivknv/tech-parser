19e11057b2886d38c702e650abded770dda9ed2e
Ivan Konovalov <ivknv0@gmail.com>
2014-10-20 12:39:54 +0600

    Improve Mashable parser

--- TechParser/mashable.py
+++ TechParser/mashable.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
 from TechParser import parser
 
 def get_articles():
-	g = grab.Grab()
-	
-	parser.setup_grab(g)
-	
-	g.go("http://mashable.com")
-	
-	css_path = "article header hgroup h1 a"
-	summary_path = "article .article-content p.article-excerpt"
-	
-	posts = parser.get_articles(g, css_path, css_path,
-		"mashable", "mashable.com", summary_path)
-	
-	return posts
+	return parser.get_articles_from_rss('http://mashable.com/rss', 'mashable')
