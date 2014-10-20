9670b5435beb98caca764a5af7e762817a9c82b0
Ivan Konovalov <ivknv0@gmail.com>
2014-10-20 13:26:21 +0600

    Improve Flowa parser

--- TechParser/flowa.py
+++ TechParser/flowa.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
 from TechParser import parser
 
 def get_articles():
-	g = grab.Grab()
-	parser.setup_grab(g)
-	
-	g.go("http://flowa.fi")
-	
-	link_path = ".article-container a"
-	title_path = link_path + " .article .article-heading .heading-container h2"
-	
-	posts = parser.get_articles(g, link_path, title_path, "flowa")
-	
-	link_path = ".main-article a:not(#blog-entries-top)"
-	title_path = link_path + " div .article-par .article-par-container .heading-container h2"
-	summary_path = link_path + " div .article-par .article-par-container .teaser"
-	
-	posts += parser.get_articles(g, link_path, title_path,
-		"flowa", "flowa.fi", summary_path)
-	
-	return posts
+	return parser.get_articles_from_rss('http://flowa.fi/rss.xml', 'flowa')
