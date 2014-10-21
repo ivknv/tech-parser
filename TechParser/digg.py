db744ff62439c784fb8aff5d43f553e8fbcd2696
Ivan Konovalov <ivknv0@gmail.com>
2014-10-21 17:00:05 +0600

    Improve Digg parser

--- TechParser/digg.py
+++ TechParser/digg.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
 from TechParser import parser
 
 def get_articles():
-	g = grab.Grab()
-	parser.setup_grab(g)
-	
-	g.go("http://digg.com")
-	
-	css_path = ".story-container .story-content .story-header .story-title .story-title-link"
-	summary_path = ".story-container .story-content p.story-description"
-	
-	posts = parser.get_articles(g, css_path, css_path,
-		"digg", summary_path=summary_path)
-	
-	return posts
+	return parser.get_articles_from_rss('http://digg.com/rss/top.rss', 'digg')
