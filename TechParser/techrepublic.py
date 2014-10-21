ee2546276b758748c4a954898db2c6e58a375a98
SPython <rvan.mega@gmail.com>
2014-10-21 12:42:32 +0600

    Improve Tech Republic parser

--- TechParser/techrepublic.py
+++ TechParser/techrepublic.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
 from TechParser import parser
 
 def get_articles():
-	g = grab.Grab()
-	
-	parser.setup_grab(g)
-	
-	g.go("http://techrepublic.com")
-	
-	posts = []
-	
-	css_paths = ["#article-river #tab-content-1 .items li .item-content .title a",
-		"#article-river #tab-content-2 .items li .item-content .title a",
-		"#haccordion .viewport section .slide .lead-in h3 a",
-		"#feature-hubs .item-list .row .item .title a"]
-	
-	for css_path in css_paths:
-		posts += parser.get_articles(g, css_path, css_path,
-			"techrepublic", "techrepublic.com")
-	
-	return posts
+	return parser.get_articles_from_rss(
+		'http://www.techrepublic.com/rssfeeds/articles/latest/',
+		'techrepublic')
