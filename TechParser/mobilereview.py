b03b4f158c7fcd001f8dab8e24f24aa718be895b
SPython <rvan.mega@gmail.com>
2014-10-21 19:56:18 +0600

    Minor fix

--- TechParser/mobilereview.py
+++ TechParser/mobilereview.py
@@ -9,6 +9,6 @@ def get_articles():
 			'http://mobile-review.com.feedsportal.com/c/33244/f/557686/index.rss',
 			'http://mobile-review.com.feedsportal.com/c/33244/f/557683/index.rss']
 	for url in urls:
-		articles += parser.get_articles_from_rss(url, 'mobilereview')
+		articles += parser.get_articles_from_rss(url, 'mobile-review')
 	
 	return articles
