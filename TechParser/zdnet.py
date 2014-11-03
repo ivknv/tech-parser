940a82385e59d2745992c114bb83cd22e208a034
SPython <rvan.mega@gmail.com>
2014-11-03 22:17:43 +0500

    Improve ZDNet parser

--- TechParser/zdnet.py
+++ TechParser/zdnet.py
@@ -3,14 +3,18 @@
 
 from TechParser import parser
 
-def get_articles():
-	urls = ['http://www.zdnet.com/news/rss.xml',
-		'http://downloads.zdnet.com/recent/?mode=rss',
-		'http://www.zdnet.com/reviews/rss.xml']
+def get_articles(categories=['all']):
+	urls = {'news': 'http://www.zdnet.com/news/rss.xml',
+		'downloads': 'http://downloads.zdnet.com/recent/?mode=rss',
+		'reviews': 'http://www.zdnet.com/reviews/rss.xml'}
+	
+	if 'all' in categories:
+		categories = ['news', 'downloads', 'reviews']
 	
 	articles = []
 	
-	for url in urls:
+	for categorie in categories:
+		url = urls[categorie]
 		for article in parser.get_articles_from_rss(url, 'zdnet'):
 			if not article in articles:
 				articles.append(article)
