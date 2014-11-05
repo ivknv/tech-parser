a531db7af151198f1e899bce73d2460dec846271
Ivan Konovalov <ivknv0@gmail.com>
2014-11-05 13:09:07 +0500

    Improve Trashbox parser

--- TechParser/trashbox.py
+++ TechParser/trashbox.py
@@ -3,6 +3,23 @@
 
 from TechParser import parser
 
-def get_articles():
-	return parser.get_articles_from_rss('http://trashbox.ru/feed_topics/1',
-		'trashbox')
+def get_articles(categories=['all']):
+	urls = {'articles': 'b_text', 'news': 'b_news', 'all': '1',
+		'games': 'games', 'programs': 'progs', 'themes': 'themes',
+		'questions': 'b_questions', 'main_page': '1/?approved'}
+	
+	if 'all' in categories:
+		selected_urls = [urls['all']]
+	else:
+		selected_urls = [urls[i] for i in categories if i in categories]
+	
+	articles = []
+	append = articles.append #OPTIMISATION
+	
+	for url in selected_urls:
+		url_ = 'http://trashbox.ru/feed_topics/{0}'.format(url)
+		for article in parser.get_articles_from_rss(url_, 'trashbox'):
+			if article not in articles:
+				append(article)
+
+	return articles
