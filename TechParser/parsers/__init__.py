e8cf41c80fd64a4467d4615090298463b5b591ae
SPython <rvan.mega@gmail.com>
2014-12-30 18:58:59 +0500

    Move parsers to parsers directory

---
+++ TechParser/parsers/__init__.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.realpath(__file__))
+
+from TechParser.parsers import habrahabr, venturebeat, engadget, slashdot
+from TechParser.parsers import gizmodo, techcrunch, readwrite
+from TechParser.parsers import techrepublic, smashingmagazine, androidcentral
+from TechParser.parsers import verge, topdesignmag, flowa, ittoolbox
+from TechParser.parsers import dzone, codeproject, hackernews, mashable
+from TechParser.parsers import mashable, maketecheasier, digg, wired
+from TechParser.parsers import medium, planetclojure, reddit, trashbox
+from TechParser.parsers import droider, redroid, threednews, ixbt, mobilereview
+from TechParser.parsers import helpix, recode, zdnet, geektimes
