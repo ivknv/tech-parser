c01ca8a2fd5fd8bb14113f3b419b350f6539884e
Ivan Konovalov <ivknv0@gmail.com>
2015-02-13 19:29:20 +0500

    Use RSS feed for parsing articles from planet.clojure.in

--- TechParser/parsers/planetclojure.py
+++ TechParser/parsers/planetclojure.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import grab
-from TechParser import parser
-from TechParser.py2x import unicode_
-from lxml.html import tostring
+from TechParser.parser import get_articles_from_rss
 
 def get_articles():
-	g = grab.Grab()
-	parser.setup_grab(g)
-	
-	g.go('http://planet.clojure.in')
-	
-	css_path = '.entry .article > h2 a'
-	summary_texts = []
-	for elem in g.css_list(".entry .article"):
-		text = ''
-		for children in elem.getchildren()[1:-1]:
-			text += parser.remove_bad_tags(tostring(children).decode())
-		summary_texts.append(text)
-			
-	posts = parser.get_articles(g, css_path, css_path,
-		'planetclojure', 'planet.clojure.in')
-	
-	for (post, summary_text) in zip(posts, summary_texts):
-		post['summary'] = summary_text
-	
-	return posts
+    return get_articles_from_rss('http://planet.clojure.in/atom.xml', 'planetclojure')
