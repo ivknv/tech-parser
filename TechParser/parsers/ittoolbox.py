13b0a2271a5a0b392941975e11536a11ae4be09c
SPython <rvan.mega@gmail.com>
2015-02-19 21:23:38 +0500

    Fix bug; Tabs to spaces

--- TechParser/parsers/ittoolbox.py
+++ TechParser/parsers/ittoolbox.py
@@ -5,12 +5,12 @@ import grab
 from TechParser import parser
 
 def get_articles():
-	g = grab.Grab()
-	
-	g.go("http://it.toolbox.com")
-	
-	css_path = ".tile .tileContent div:first-child .floatleft:nth-child(2) a"
-	
-	posts = parser.get_articles(g, css_path, css_path, "ittoolbox")
-	
-	return posts
+    g = grab.Grab()
+    g.config['connect_timeout'] = 15
+    g.go("http://it.toolbox.com")
+    
+    css_path = ".tile .tileContent div:first-child .floatleft:nth-child(2) a"
+    
+    posts = parser.get_articles(g, css_path, css_path, "ittoolbox")
+    
+    return posts
